114f9c075449467003d08c369930e015bcc8afa3
Bimo <ruixutcd@gmail.com>
2021-04-14 00:45:52 +0100

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install openjdk-8-jdk -y
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y
 sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 40x main" > /etc/apt/sources.list.d/cassandra.list'
 wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt update
