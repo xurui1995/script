89696a18b19fd6db89b23005592c202a1580c737
Bimo <ruixutcd@gmail.com>
2021-04-14 00:39:25 +0100

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -4,6 +4,6 @@ sudo apt install apt-transport-https
 sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 40x main" > /etc/apt/sources.list.d/cassandra.list'
 wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt update
-sudo apt install ntp
-sudo apt install cassandra
+sudo apt install ntp -y
+sudo apt install cassandra -y
 sudo systemctl stop cassandra
