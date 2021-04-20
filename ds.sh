96d44140e5a5a7e7b746734231dec326ebfb25ce
Bimo <ruixutcd@gmail.com>
2021-04-20 01:31:40 +0100

    Update ds.sh

--- ds.sh
+++ ds.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install openjdk-8-jdk -y
 sudo apt install apt-transport-https -y
-sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 40x main" > /etc/apt/sources.list.d/cassandra.list'
+sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 311x main" > /etc/apt/sources.list.d/cassandra.list'
 wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt update
 sudo apt install ntp -y
